8d6f1e9332705ff96949c5d4a19ab6de111a2c0f
TimTmi <khoanart05@gmail.com>
2026-09-16 23:08:25 +0700

    feat: add HandController

---
+++ Assets/Features/Player/HandController.cs
@@ -0,0 +1,12 @@
+using Features.Pickables;
+using UnityEngine;
+
+namespace Features.Player
+{
+    public class HandController : MonoBehaviour
+    {
+        private PickableData _heldPickableData;
+        public PickableData HeldPickableData => _heldPickableData;
+        private GameObject _heldPrefab;
+    }
+}
