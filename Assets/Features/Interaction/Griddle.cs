97de42a3c97eb5bd384bfbdd830a316445bc99ae
TimTmi <khoanart05@gmail.com>
2026-09-20 00:44:20 +0700

    feat: implement griddle

--- Assets/Features/Interaction/Griddle.cs
+++ Assets/Features/Interaction/Griddle.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using Features.Ingredient;
 using UnityEngine;
 
@@ -5,20 +6,77 @@ namespace Features.Interaction
 {
     public class Griddle : MonoBehaviour, IInteractable
     {
+        [SerializeField] private Vector3 defaultRotation = new Vector3(-90f, 0f, 0f);
+        [SerializeField] private Vector3 arrangementStart;
+        [SerializeField] private Vector3 arrangementDirection = Vector3.right;
+
+        private readonly List<Pickup.Ingredient> _cooking = new();
+
         public bool CanInteract(in InteractionContext context)
         {
-            var pickable = context.HeldPickable;
-            return pickable is Pickup.Ingredient ingredient && ingredient.IngredientData.HasProcess(ProcessType.Grill);
+            return context.HeldPickable is Pickup.Ingredient;
         }
 
         public void Interact(in InteractionContext context)
         {
-            throw new System.NotImplementedException();
+            var ingredient = (Pickup.Ingredient)context.HeldPickable;
+
+            ingredient.transform.SetParent(transform);
+            ingredient.transform.SetLocalPositionAndRotation(ArrangementPosition(_cooking.Count), Quaternion.Euler(defaultRotation));
+            _cooking.Add(ingredient);
+
+            context.Release();
         }
 
         public string GetInteractionPrompt(in InteractionContext context)
         {
             return "Grill";
+        }
+
+        private void Update()
+        {
+            for (int i = _cooking.Count - 1; i >= 0; i--)
+            {
+                Pickup.Ingredient ingredient = _cooking[i];
+
+                if (ingredient == null || !ingredient.transform.IsChildOf(transform))
+                {
+                    _cooking.RemoveAt(i);
+                    continue;
+                }
+
+                if (!ingredient.IngredientData.TryGetProcess(ProcessType.Grill, out IngredientProcess process)
+                    || ingredient.CookingProgress >= process.Duration)
+                {
+                    continue;
+                }
+
+                ingredient.AddCookingProgress(Time.deltaTime);
+
+                if (ingredient.CookingProgress >= process.Duration)
+                {
+                    ReplaceWithResults(ingredient, process);
+                }
+            }
+        }
+
+        private Vector3 ArrangementPosition(int slot)
+        {
+            return arrangementStart + arrangementDirection * slot;
+        }
+
+        private void ReplaceWithResults(Pickup.Ingredient ingredient, IngredientProcess process)
+        {
+            int slot = _cooking.IndexOf(ingredient);
+            _cooking.Remove(ingredient);
+            Destroy(ingredient.gameObject);
+
+            for (int i = 0; i < process.Result.Length; i++)
+            {
+                Pickup.Ingredient result = Instantiate(process.Result[i], transform);
+                result.transform.SetLocalPositionAndRotation(ArrangementPosition(slot + i), Quaternion.Euler(defaultRotation));
+                _cooking.Add(result);
+            }
         }
     }
 }
