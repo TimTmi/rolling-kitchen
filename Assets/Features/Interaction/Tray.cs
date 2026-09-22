d682560f0211d41546de06b9403fd2929e706842
TimTmi <khoanart05@gmail.com>
2026-09-23 00:12:24 +0700

    feat: allow placing on tray

--- Assets/Features/Interaction/Tray.cs
+++ Assets/Features/Interaction/Tray.cs
@@ -1,22 +1,40 @@
+using Features.Pickup;
 using UnityEngine;
 
 namespace Features.Interaction
 {
     public class Tray : MonoBehaviour, IInteractable
     {
+        [SerializeField] private Vector3 defaultRotation = new Vector3(-90f, 0f, 0f);
+        [SerializeField] private Vector3 arrangementStart;
+        [SerializeField] private Vector3 arrangementDirection = Vector3.right;
+        [SerializeField] private int maxSlots = 8;
+
+        private IngredientSlots _slots;
+
         public bool CanInteract(in InteractionContext context)
         {
-            return context.HeldPickable != null;
+            _slots.ClearDetached();
+
+            return context.HeldPickable != null && _slots.HasFreeSlot;
         }
 
         public void Interact(in InteractionContext context)
         {
-            
+            Pickable content = context.HeldPickable is IngredientStack stack ? stack.Root : context.HeldPickable;
+            _slots.Place(content);
+
+            context.Release();
         }
 
         public string GetInteractionPrompt(in InteractionContext context)
         {
             return "Place";
+        }
+
+        private void Awake()
+        {
+            _slots = new IngredientSlots(transform, defaultRotation, arrangementStart, arrangementDirection, maxSlots);
         }
     }
 }
