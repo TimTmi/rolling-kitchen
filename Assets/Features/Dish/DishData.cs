eb8940a9310639f384d55de3b0764dc37d385cf4
TimTmi <khoanart05@gmail.com>
2026-09-23 17:20:07 +0700

    feat: add box to dish data

--- Assets/Features/Dish/DishData.cs
+++ Assets/Features/Dish/DishData.cs
@@ -10,13 +10,31 @@ namespace Features.Dish
     [CreateAssetMenu(fileName = "DishData", menuName = "Scriptable Objects/DishData")]
     public class DishData : ScriptableObject
     {
+        [SerializeField] private Pickable box;
         [SerializeField] private Pickup.Ingredient baseIngredient;
         [SerializeField] private Pickup.Ingredient topIngredient;
         [SerializeField] private Pickup.Ingredient[] toppings = Array.Empty<Pickup.Ingredient>();
 
+        public Pickable Box => box;
         public Pickup.Ingredient BaseIngredient => baseIngredient;
         public Pickup.Ingredient TopIngredient => topIngredient;
         public Pickup.Ingredient[] Toppings => toppings;
+
+        public IEnumerable<Pickable> RequiredPickables
+        {
+            get
+            {
+                if (box != null)
+                {
+                    yield return box;
+                }
+
+                foreach (Pickup.Ingredient ingredient in RequiredIngredients)
+                {
+                    yield return ingredient;
+                }
+            }
+        }
 
         public IEnumerable<Pickup.Ingredient> RequiredIngredients
         {
