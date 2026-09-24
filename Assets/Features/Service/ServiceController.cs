36fdb2ff3023c7decd94c4b07ce1dcb92ba955c2
TimTmi <khoanart05@gmail.com>
2026-09-25 00:39:10 +0700

    refactor: move order orchestration logic from OrderManager to ServiceController

--- Assets/Features/Service/ServiceController.cs
+++ Assets/Features/Service/ServiceController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Features.Dish;
 using Features.Interaction;
 using Features.Pickup;
 using Features.Player;
@@ -22,13 +24,101 @@ namespace Features.Service
         [SerializeField] private Core.PoiCameraController poiCameraController;
         [SerializeField] private Camera playerCamera;
         [SerializeField] private Camera poiCamera;
+        [SerializeField] private OrderManager orderManager;
+        [SerializeField] private DishData[] dishes = Array.Empty<DishData>();
+        [SerializeField] private float minFreeTime = 5f;
+        [SerializeField] private float maxFreeTime = 10f;
+        [SerializeField] private int maxOrderSize = 1;
 
         private Action<Pickable> _selectionHandler;
         private bool _poiFocusCancellable;
+        private float[] _spawnTimers;
 
         public event Action PoiFocusEnded;
 
         public Camera PoiCamera => poiCamera;
+
+        private void Update()
+        {
+            EnsureSpawnTimersInitialized();
+            for (int i = 0; i < _spawnTimers.Length; i++)
+            {
+                if (orderManager.GetOrder(i) != null)
+                {
+                    continue;
+                }
+
+                _spawnTimers[i] -= Time.deltaTime;
+                if (_spawnTimers[i] > 0f)
+                {
+                    continue;
+                }
+
+                Order order = GenerateRandomOrder();
+                if (order != null)
+                {
+                    orderManager.TryPlaceOrder(i, order);
+                }
+
+                _spawnTimers[i] = RandomSpawnDelay();
+            }
+        }
+
+        private void EnsureSpawnTimersInitialized()
+        {
+            if (_spawnTimers == null || _spawnTimers.Length != orderManager.SlotCount)
+            {
+                _spawnTimers = new float[orderManager.SlotCount];
+                for (int i = 0; i < _spawnTimers.Length; i++)
+                {
+                    _spawnTimers[i] = RandomSpawnDelay();
+                }
+            }
+        }
+
+        private Order GenerateRandomOrder()
+        {
+            if (dishes.Length == 0)
+            {
+                return null;
+            }
+
+            int dishCount = UnityEngine.Random.Range(1, maxOrderSize + 1);
+            DishData[] orderDishes = new DishData[dishCount];
+            for (int i = 0; i < dishCount; i++)
+            {
+                orderDishes[i] = GenerateRandomDish();
+            }
+
+            return new Order(orderDishes);
+        }
+
+        private DishData GenerateRandomDish()
+        {
+            if (dishes.Length == 0)
+            {
+                return null;
+            }
+
+            DishData dish = dishes[UnityEngine.Random.Range(0, dishes.Length)];
+            if (dish.Toppings.Length == 0)
+            {
+                return dish;
+            }
+
+            DishData randomized = Instantiate(dish);
+            randomized.name = dish.name;
+            randomized.SetToppings(dish.Toppings
+                .OrderBy(_ => UnityEngine.Random.value)
+                .Take(UnityEngine.Random.Range(1, dish.Toppings.Length + 1))
+                .ToArray());
+            return randomized;
+        }
+
+        private float RandomSpawnDelay()
+        {
+            return UnityEngine.Random.Range(minFreeTime, maxFreeTime);
+        }
 
         void Start()
         {
